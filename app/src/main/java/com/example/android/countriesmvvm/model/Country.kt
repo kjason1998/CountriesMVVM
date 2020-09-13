931cb2ad64800961b853be2de8a532b0630242b0
kjason1998 <kjason1998@gmail.com>
2020-09-14 00:27:57 +0100

    update country data not is has 3 variables and we use SerializeName annotation to link the json file data to this model

--- app/src/main/java/com/example/android/countriesmvvm/model/Country.kt
+++ app/src/main/java/com/example/android/countriesmvvm/model/Country.kt
@@ -1,3 +1,15 @@
 package com.example.android.countriesmvvm.model
 
-data class Country(val countryName: String?)
+import com.google.gson.annotations.SerializedName
+
+data class Country(
+    // this will map json name value to countryName
+    @SerializedName("name")
+    val countryName: String?,
+
+    @SerializedName("capital")
+    val countryCapital: String?,
+
+    @SerializedName("flagPNG")
+    val countryFlagUrl: String?
+)
