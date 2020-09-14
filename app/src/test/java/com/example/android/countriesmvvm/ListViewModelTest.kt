ef1b3b97cc9886ec93772711fe347ac0be93ddcf
kjason1998 <kjason1998@gmail.com>
2020-09-14 18:11:43 +0100

    Make the actual test

--- app/src/test/java/com/example/android/countriesmvvm/ListViewModelTest.kt
+++ app/src/test/java/com/example/android/countriesmvvm/ListViewModelTest.kt
@@ -1,17 +1,58 @@
 package com.example.android.countriesmvvm
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.countriesmvvm.model.CountriesService
+import com.example.android.countriesmvvm.model.Country
+import com.example.android.countriesmvvm.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 
 class ListViewModelTest {
+
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    @InjectMocks
+    val listViewModel = ListViewModel()
+
+    private var testSingle: Single<List<Country>>? = null
+
+    @Before
+    fun setup(){
+        MockitoAnnotations.initMocks(this)
+    }
+
+    // the success path
+    @Test // test annotation to know that this  will be run as a test
+    fun getCountriesSuccess(){
+        val country = Country("Country name","Capital","url")
+        val countryList = arrayListOf(country)
+
+        testSingle = Single.just(countryList)
+
+        Mockito.`when`(countriesService.getCountries()).thenReturn((testSingle))
+
+        listViewModel.refresh() // testing the functionality
+
+        // check if all the value is correct
+        Assert.assertEquals(1, listViewModel.countries.value?.size) // testing the size is one
+        Assert.assertEquals(false,listViewModel.countryLoadError.value)
+        Assert.assertEquals(false,listViewModel.loading.value)
+    }
 
     @get:Rule
     var rule = InstantTaskExecutorRule()
