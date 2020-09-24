089991b348716b8233f26fd1bca23b86e27b050c
kjason1998 <kjason1998@gmail.com>
2020-09-25 00:20:30 +0100

    Fix testing does not have @Before

--- app/src/test/java/com/example/android/countriesmvvm/ListViewModelTest.kt
+++ app/src/test/java/com/example/android/countriesmvvm/ListViewModelTest.kt
@@ -1,5 +1,6 @@
-package com.example.android.countriesmvvm
+package com.devtides.countries
 
+import android.accounts.NetworkErrorException
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.android.countriesmvvm.model.CountriesService
 import com.example.android.countriesmvvm.model.Country
@@ -17,77 +18,82 @@ import org.junit.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import java.lang.Exception
+import java.util.ArrayList
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 
 class ListViewModelTest {
 
-    @Mock
-    lateinit var countriesService: CountriesService
-
-    @InjectMocks
-    val listViewModel = ListViewModel()
-
-    private var testSingle: Single<List<Country>>? = null
-
     @Before
-    fun setup(){
+    fun setup() {
         MockitoAnnotations.initMocks(this)
-    }
-
-    // the success path
-    @Test // test annotation to know that this  will be run as a test
-    fun getCountriesSuccess(){
-        val country = Country("Country name","Capital","url")
-        val countryList = arrayListOf(country)
-
-        testSingle = Single.just(countryList)
-
-        Mockito.`when`(countriesService.getCountries()).thenReturn((testSingle))
-
-        listViewModel.refresh() // testing the functionality
-
-        // check if all the value is correct
-        Assert.assertEquals(1, listViewModel.countries.value?.size) // testing the size is one
-        Assert.assertEquals(false,listViewModel.countryLoadError.value)
-        Assert.assertEquals(false,listViewModel.loading.value)
     }
 
     @get:Rule
     var rule = InstantTaskExecutorRule()
 
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    // list view model that is connect to the mock that we create in the @test function
+    @InjectMocks
+    var listViewModel = ListViewModel()
+
+
+    private var testSingle: Single<List<Country>>? = null
+
+    @Test
+    fun getCountriesSuccess() {
+        val country = Country("countryName", "capital", "url")
+        val countriesList = arrayListOf(country)
+
+        testSingle = Single.just(countriesList)
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.countries.value?.size)
+        Assert.assertEquals(false, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
+
+    @Test
+    fun getCountriesFailed() {
+
+        val errorException = NetworkErrorException("Network issue")
+
+        testSingle = Single.error(errorException)
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(null, listViewModel.countries.value?.size)
+        Assert.assertEquals(true, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
+
+
     @Before
-    fun SetUpRxSchedulers(){
+    fun setUpRxSchedulers() {
         val immediate = object : Scheduler() {
-            // make sure delay is zero because we do not want any delay when calling schedulers
             override fun scheduleDirect(run: Runnable?, delay: Long, unit: TimeUnit?): Disposable {
                 return super.scheduleDirect(run, 0, unit)
             }
 
             override fun createWorker(): Worker {
-                return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
+                return ExecutorScheduler.ExecutorWorker { it.run() }
             }
         }
 
-        RxJavaPlugins.setInitIoSchedulerHandler{
-            Scheduler -> immediate
-        }
-        RxJavaPlugins.setInitComputationSchedulerHandler(){
-                Scheduler -> immediate
-        }
-        RxJavaPlugins.setInitNewThreadSchedulerHandler(){
-                Scheduler -> immediate
-        }
-        RxJavaPlugins.setInitSingleSchedulerHandler(){
-                Scheduler -> immediate
-        }
-        RxAndroidPlugins.setInitMainThreadSchedulerHandler(){
-                Scheduler -> immediate
-        }
-
-
-
-
+        RxJavaPlugins.setInitIoSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitComputationSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitSingleSchedulerHandler { scheduler -> immediate }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> immediate }
     }
 }
